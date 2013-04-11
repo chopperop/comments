4b189c1ff0a1e5c52606eb011620ec6cdcb26f36
David <david.wu712@gmail.com>
2013-04-11 00:43:43 -0400

    Switched from uglifier to closure to precompile assets. Precompiled assets.

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ group :assets do
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
 
-  gem 'uglifier'
+  gem 'closure-compiler'
 
   gem 'angularjs-rails'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,8 @@ Comments::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  config.assets.js_compressor = :closure
+
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
    config.assets.precompile += %w( bootstrap2.css )
